6107b5f64075d3eecc3ce9f1556bb45487328f9f
sarziv <saruzivila12@gmail.com>
2019-06-30 23:41:11 +0300

    test9

--- config/database.php
+++ config/database.php
@@ -62,21 +62,7 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
-        'mysql_testing' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'homestead_test'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'collation' => 'utf8_unicode_ci',
-            'prefix' => '',
-            'strict' => false,
-            'engine' => null,
-        ],
-
+        
 
         'pgsql' => [
             'driver' => 'pgsql',

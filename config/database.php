450e75bbcf3ee0bdeb60f0b6de3188feb096f605
sarziv <saruzivila12@gmail.com>
2019-06-30 21:28:24 +0300

    travis .env

--- config/database.php
+++ config/database.php
@@ -65,10 +65,11 @@ return [
 
         'testing' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', '127.0.0.1'),
+            'host'      => env('DB_HOST', 'localhost'),
+            'port'      => env('DB_PORT', '3306'),
             'database'  => env('DB_DATABASE', 'homestead_test'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', 'secret'),
+            'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',

62f8a0db857d6317b0a0920a8f6615b8bc70089d
sarziv <saruzivila12@gmail.com>
2019-06-30 21:03:13 +0300

    homestead_test database

--- config/database.php
+++ config/database.php
@@ -65,10 +65,10 @@ return [
 
         'testing' => [
             'driver'    => 'mysql',
-            'host'      => env('DB_HOST', 'localhost'),
-            'database'  => env('DB_DATABASE', 'homestead'),
+            'host'      => env('DB_HOST', '127.0.0.1'),
+            'database'  => env('DB_DATABASE', 'homestead_test'),
             'username'  => env('DB_USERNAME', 'root'),
-            'password'  => env('DB_PASSWORD', ''),
+            'password'  => env('DB_PASSWORD', 'secret'),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',

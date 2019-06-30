3e293c72d8fc940af9287bb9823d462901ba800b
sarziv <saruzivila12@gmail.com>
2019-07-01 00:17:19 +0300

    mysql integretion

--- config/database.php
+++ config/database.php
@@ -62,7 +62,17 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
-
+        'testing' => [
+            'driver'    => 'mysql',
+            'host'      => env('DB_TEST_HOST', 'localhost'),
+            'port'      => env('DB_PORT', '3306'),
+            'database'  => env('DB_TEST_DATABASE', 'homestead_test'),
+            'username'  => env('DB_TEST_USERNAME', 'root'),
+            'password'  => env('DB_TEST_PASSWORD', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix'    => '',
+            ],
 
         'pgsql' => [
             'driver' => 'pgsql',
